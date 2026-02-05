0cb178a2ebe78ac33574a3d7106ea8dc35b488e3
Ruby_on_Rails Developer <egorarbuzov@gmail.com>
2026-02-05 11:42:56 +0300

    add articles pagination

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_23_105046) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_05_070816) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -82,6 +82,12 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_23_105046) do
     t.string "title"
     t.text "body"
     t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
