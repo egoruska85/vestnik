8b2d6d8f190be9caada70e67342ce4083e169850
Ruby_on_Rails Developer <egorarbuzov@gmail.com>
2026-02-05 15:19:59 +0300

    add ads

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,9 @@ class MainController < ApplicationController
   def index
     @first_slide = Mainbanner.first
     @slides = Mainbanner.where(id: 2..100)
+
+    @ads = Ad.page(params[:page])
+
+    @categories = Category.all
   end
 end
