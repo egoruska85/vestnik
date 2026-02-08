998adc2d632c59fed15b1a5b837177d466932f8f
Ruby_on_Rails Developer <egorarbuzov@gmail.com>
2026-02-08 23:07:28 +0300

    add show ad

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,20 @@ import "controllers"
 
 import "popper"
 import "bootstrap"
+
+
+const preview = document.getElementById('previewImage');
+const thumbs = document.querySelectorAll('.thumb');
+
+thumbs.forEach(thumb => {
+  thumb.addEventListener('click', () => {
+    // меняем картинку
+    preview.src = thumb.src;
+
+    // убираем active у всех
+    thumbs.forEach(t => t.classList.remove('active'));
+
+    // добавляем active выбранной
+    thumb.classList.add('active');
+  });
+});
