7dddac2e23a4edc2aba0bd8afe997dbcbeca638a
Ruby_on_Rails Developer <egorarbuzov@gmail.com>
2026-02-09 13:52:19 +0300

    refactoring code auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,11 +29,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    def configure_permitted_parameters
-      added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-      devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
-      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    end
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :phone, :password])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,27 +3,21 @@ class User < ApplicationRecord
   has_many :ads
   has_one_attached :avatar
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, # ... ваши модули
+         authentication_keys: [:login]
 
-  def login
-    @login || self.username || self.email || self.phone
-  end
+  attr_accessor :login
 
+  # Метод позволяет искать пользователя по трем полям
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if (login = conditions.delete(:login))
-      where(conditions.to_h).where(["lower(username) = :value OR lower(phone) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email) || conditions.has_key?(:email)
+      where(conditions.to_h).where([
+        "lower(username) = :value OR lower(email) = :value OR phone = :value",
+        { value: login.downcase }
+      ]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email) || conditions.has_key?(:phone)
       where(conditions.to_h).first
-    end
-  end
-
-  def validate_username
-    if User.where(email: username).exists? or User.where(email: phone).exists?
-      errors.add(:username, :invalid)
     end
   end
 end
