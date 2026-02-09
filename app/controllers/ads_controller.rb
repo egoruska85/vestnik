14573992cf1d53646e236f4549e4bde5c872b2c6
Ruby_on_Rails Developer <egorarbuzov@gmail.com>
2026-02-09 22:11:12 +0300

    add button delete ad

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,4 +1,15 @@
 class AdsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @ad = Ad.new(ad_params)
+    @ad.user_id = current_user.id
+    @ad.save
+    redirect_to @ad
+  end
   def show
     @ad = Ad.find(params[:id])
   end
@@ -16,6 +27,16 @@ class AdsController < ApplicationController
       redirect_to @ad
     else
       render action: 'edit'
+    end
+  end
+
+  def destroy
+    @ad = Ad.find(params[:id])
+    @ad.destroy
+
+    respond_to do |format|
+      format.html { redirect_to ads_path, status: :see_other, notice: "Запись удалена" }
+      format.json { head :no_content }
     end
   end
 
